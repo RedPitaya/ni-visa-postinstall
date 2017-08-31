5ac39f9913db395e1814e5eca76f6b766437b2e2
Iztok Jeras <iztok.jeras@gmail.com>
2017-08-31 12:26:50 +0200

    initial commit of code from: https://forums.ni.com/t5/Linux-Users/Using-NI-VISA-with-Arch-Linux-or-Ubuntu-14-04/gpm-p/3462361#M2287

---
+++ post_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+register() {
+    install -b -m755 ./init.d/nipal /usr/local/natinst/nipal/etc/init.d/nipal
+    install -b -m644 ./menus/*.menu /etc/xdg/menus/applications-merged/
+    install -b -m644 ./services/*.service /etc/systemd/system/
+    chmod a-x /etc/init.d/*
+    systemctl daemon-reload
+    systemctl enable nipal && echo "Enabled nipal.service"
+}
+
+register && cat << EOF
+
+-------------------------------------------------------------------------------
+
+Registered systemd services.
+
+To modify service(s) , use:
+    systemctl (start|stop|status|enable) (nilxid|nipal|nisvcloc|nimdnsd|nipxirmu).service
+
+-------------------------------------------------------------------------------
+
+EOF
+
